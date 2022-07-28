4a5c4256fdeb9e9bfd2c4f847d5755dd368c213d
Ansis Maleckis <maleckisansis@gmail.com>
2022-07-28 10:03:19 +0300

    passed but still need some refractoring

--- src/carsharing/Main.java
+++ src/carsharing/Main.java
@@ -15,7 +15,6 @@ public class Main {
     public static void main(String[] args) {
 
         /* Get commandline argument and construct database URL*/
-
         if(args.length > 1 && args[0].equals("-databaseFileName")) {
             fileName = args[1];
             jdbcURL = "jdbc:h2:./src/carsharing/db/" + args[1];
@@ -25,8 +24,10 @@ public class Main {
             jdbcURL = "jdbc:h2:./src/carsharing/db/carsharing";
         }
 
-        /* Enter infinite loop */
+        /* Initialize the database */
+        createTable();
 
+        /* Enter infinite loop */
         scanner = new Scanner(System.in);
         while(true){
             System.out.println("1. Log in as a manager\n" +
@@ -38,18 +39,41 @@ public class Main {
                 LogInAsManager();
         }
         scanner.close();
+    }
 
+    private static void createTable() {
+
+        try{
+            Class.forName ("org.h2.Driver");
+            Connection connection = DriverManager.getConnection(jdbcURL);
+            connection.setAutoCommit(true);
+            Statement st = connection.createStatement();
+            /* Check if table exists. If it doesn't create it. */
+            DatabaseMetaData metaData = connection.getMetaData();
+            ResultSet resultSet = metaData.getTables(null, null, tableName, new String[] {"TABLE"});
+            if(!resultSet.next()){
+                st.executeUpdate("CREATE TABLE " + tableName + " (ID int auto_increment primary key, NAME varchar(250) unique not null);");
+            }
+            connection.close();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void LogInAsManager() {
         
         while(true){
-            System.out.println("1. Company list\n" +
+            System.out.println("\n1. Company list\n" +
                     "2. Create a company\n" +
                     "0. Back");
             String input = scanner.nextLine();
-            if (input.equals("0"))
+            if (input.equals("0")) {
+                System.out.println();
                 return;
+            }
             else if  (input.equals("1"))
                 PrintCompanyList();
             else if (input.equals("2"))
@@ -59,7 +83,7 @@ public class Main {
 
     private static void CreateCompany() {
 
-        System.out.println("Enter the company name:");
+        System.out.println("\nEnter the company name:");
         String companyName = scanner.nextLine();
 
         try{
@@ -67,22 +91,8 @@ public class Main {
             Connection connection = DriverManager.getConnection(jdbcURL);
             connection.setAutoCommit(true);
             Statement st = connection.createStatement();
-            DatabaseMetaData metaData = connection.getMetaData();
-            ResultSet resultSet = metaData.getTables(null, null, tableName, new String[] {"TABLE"});
-            if(resultSet.next()){
-                System.out.println("Table exists!!!");
-            }
-            else {
-                System.out.println("Initializing new table!!!");
-                st.executeUpdate("CREATE TABLE " + tableName + " (ID int auto_increment primary key, NAME varchar(250) unique not null);");
-            }
             st.executeUpdate("INSERT INTO " + tableName + " (NAME) VALUES ('" + companyName + "');");
-
-            //execute Q
-//            ResultSet resultSet = st.executeQuery("query_placeholder");
-//            while(resultSet.next()){
-//                String data = resultSet.getString("column_name_placeholder");
-//            }
+            System.out.println("The company was created!");
             connection.close();
         }
         catch (SQLException e) {
@@ -93,5 +103,29 @@ public class Main {
     }
 
     private static void PrintCompanyList() {
+
+        try{
+            Class.forName ("org.h2.Driver");
+            Connection connection = DriverManager.getConnection(jdbcURL);
+            connection.setAutoCommit(true);
+            Statement st = connection.createStatement();
+            ResultSet resultSet = st.executeQuery("SELECT * FROM " + tableName + ";");
+            if(resultSet.next()) {
+                System.out.println("\nCompany list:");
+                System.out.printf("%s. %s", resultSet.getString("ID"), resultSet.getString("NAME"));
+                while(resultSet.next()){
+                    System.out.printf("\n%s. %s", resultSet.getString("ID"), resultSet.getString("NAME"));
+                }
+            }
+            else
+                System.out.printf("\nThe company list is empty!");
+            System.out.println();
+            connection.close();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
